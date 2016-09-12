5b80c988cc09d76a82ca79d19f3743b8764869d3
Daniel Blancas <edblancas@gmail.com>
2016-09-12 03:33:33 -0500

    Nombres completos pelis

--- src/main/java/example/MoviesController.java
+++ src/main/java/example/MoviesController.java
@@ -10,9 +10,13 @@ public class MoviesController {
     @RequestMapping(value="/movies", method = RequestMethod.GET)
     public Movie[] movies() {
         return new Movie[]{
-                new Movie(0, "Star Wars I"),
-                new Movie(1, "Star Wars II"),
-                new Movie(1, "Star Wars III")
+                new Movie(0, "Episode I – The Phantom Menace (1999)"),
+                new Movie(1, "Star Wars II – Attack of the Clones (2002)"),
+                new Movie(2, "Star Wars II – Revenge of the Sith (2005)"),
+                new Movie(3, "Star Wars IV – A New Hope (1977)"),
+                new Movie(4, "Star Wars V – The Empire Strikes Back (1980)"),
+                new Movie(5, "Star Wars VI – Return of the Jedi (1983)"),
+                new Movie(7, "Star Wars VII – The Force Awakens (2015)"),
         };
     }
 }
